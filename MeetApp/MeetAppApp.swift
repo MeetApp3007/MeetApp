c6df90e50f0d1a0d2f72aa3e33b66739269d5c93
1inchunikhin <1inchunikhin@gmail.com>
2023-04-25 22:16:11 +0300

    Initial Commit

---
+++ MeetApp/MeetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetAppApp.swift
+//  MeetApp
+//
+//  Created by Николай Чунихин on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
